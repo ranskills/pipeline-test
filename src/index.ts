64bc890a82d3593b3b6ac6cc25e4b3378f3fef55
Ransford Okpoti <ranskills@yahoo.co.uk>
2021-05-03 16:21:11 +0000

    Add static files to binary

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/v1', (req, res) => res.send('Express + TypeScript Server'));
 
