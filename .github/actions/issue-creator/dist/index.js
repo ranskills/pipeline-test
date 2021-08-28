5ebac4a87dbcdc1a9540a0a73b7a74d04b551f47
Ransford Okpoti <ranskills@yahoo.co.uk>
2021-08-28 11:53:37 +0000

    Minor update

--- .github/actions/issue-creator/dist/index.js
+++ .github/actions/issue-creator/dist/index.js
@@ -6321,17 +6321,18 @@ try {
     const assignees = core.getInput('assignees', { trimWhitespace: true }).split(',')
 
     const octokit = github.getOctokit(GITHUB_TOKEN)
-    const { context } = github
 
-    console.log(`URL: ${context.payload.repository.url}`)
-    console.log(context.payload.repository.owner)
-    const { repository } = context.payload
+    const { context } = github
+    // const { repository } = context.payload
+
+    core.info(`RunId: ${context.runId} RunNumber: ${context.runNumber} Workflow: ${context.workflow}`)
     octokit.rest.issues.create({
         // repo: context.payload.repository.url,
-        repo: repository.name,
-        owner: repository.owner.login,
-        title: 'XXXX',
-        body: 'YYYY',
+        ...context.repo,
+        // repo: repository.name,
+        // owner: repository.owner.login,
+        title: `Pipeline ${github.workflow} Failure`,
+        body: `RunId: ${context.runId} RunNumber: ${context.runNumber} Workflow: ${context.workflow}`,
         assignees
     }).catch(error => {
         console.debug(error)
