734d01d887bda46ce8a5bfb324c888bf8ed00283
Ransford Okpoti <ranskills@yahoo.co.uk>
2021-09-27 01:02:28 +0000

    test: Update test

--- test/unit/Person.spec.ts
+++ test/unit/Person.spec.ts
@@ -5,5 +5,8 @@ test('Person', function (t: Test) {
   let person = new Person('Bond', '');
   t.equal(person.name, 'Bond');
 
+  person = new Person('Bond', 'Bond');
+  t.equal(person.name, 'Bond');
+
   t.end();
 });
